c4d80380b27eb6aa8ddac0242fdb805509a06cdb
ratautas <algirdas.tamasauskas@gmail.com>
2021-02-21 15:12:21 +0200

    Fix tab closing issues

--- background.js
+++ background.js
@@ -8,16 +8,12 @@ chrome.runtime.onMessage.addListener(({ type, payload }, { tab }, sendResponse) 
   if (type === "SET_RECIPIENT") {
     chrome.tabs.query({ currentWindow: true }, (tabs) => {
       const manosiuntosTab = tabs.find(({ url }) => url.includes("https://manosiuntos.post.lt"));
-      if (manosiuntosTab) {
-        chrome.tabs.update(manosiuntosTab.id, { active: true, highlighted: true });
-        chrome.runtime.sendMessage({ type: "OPEN_MANOSIUNTOS" });
-      } else {
-        chrome.tabs.create({
-          active: true,
-          url: "https://manosiuntos.post.lt/send/prepare?addNew=true",
-        });
-        chrome.runtime.sendMessage({ type: "CREATE_MANOSIUNTOS" });
-      }
+      if (manosiuntosTab) chrome.tabs.remove(manosiuntosTab.id);
+      chrome.tabs.create({
+        active: true,
+        url: "https://manosiuntos.post.lt/send/prepare?addNew=true",
+      });
+      chrome.runtime.sendMessage({ type: "CREATE_MANOSIUNTOS" });
     });
   }
 });
