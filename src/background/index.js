e6e6e942dba65ad2ff3f19a6980f7017a8fff2d5
ratautas <algirdas.tamasauskas@gmail.com>
2021-07-04 17:03:35 +0300

    Update API urls

--- src/background/index.js
+++ src/background/index.js
@@ -1,13 +1,13 @@
 chrome.runtime.onMessage.addListener(({ type, payload }, { tab }, sendResponse) => {
   if (type === "SET_RECIPIENT") {
     chrome.tabs.query({ currentWindow: true }, (tabs) => {
-      const manosiuntosTab = tabs.find(({ url }) => url.includes("https://manosiuntos.post.lt"));
+      const manosiuntosTab = tabs.find(({ url }) => url.includes("https://lpexpress.lt"));
 
       if (manosiuntosTab) chrome.tabs.remove(manosiuntosTab.id);
 
       chrome.tabs.create({
         active: true,
-        url: "https://manosiuntos.post.lt/send/prepare?addNew=true",
+        url: "https://lpexpress.lt/send/prepare?addNew=true",
       });
 
       chrome.runtime.sendMessage({ type: "CREATE_MANOSIUNTOS" });
